ce20796ccc2e2a4d2d43bcd13481614be539cf2f
storke <storke@umich.edu>
2021-10-07 20:48:03 -0400

    react >:(

--- insta485/js/main.jsx
+++ insta485/js/main.jsx
@@ -6,5 +6,30 @@ import Post from './post';
 ReactDOM.render(
   // Insert the post component into the DOM
   <Post url="/api/v1/posts/1/" />,
-  document.getElementById('reactEntry'),
+  entry = document.getElementById('reactEntry'),
 );
+
+
+
+function handleResponse (response) {
+  return response.json();
+}
+
+
+
+function display_posts(entry) {
+
+
+  fetch("/api/v1/posts/1/")
+  .then(handleResponse)
+
+  const posts = []
+
+  posts.forEach((post) => {
+    const n = document.createElement('p');
+    const s = `${user.username} has ${user.snippets.length} snippets`;
+    const t = document.createTextNode(s);
+    n.appendChild(t);
+    entry.appendChild(n);
+    });
+}
