4f39b527a6ae0d9f427f09fb9da9c43f9caf1c49
storke <storke@umich.edu>
2021-10-13 01:30:13 -0400

    form

--- insta485/js/main.jsx
+++ insta485/js/main.jsx
@@ -1,81 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Post from './post';
-import Batch from './middle';
-
-
-
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Batch from "./middle";
 
 // This method is only called once
 // {/* <Post url="/api/v1/posts/1/" />, */}
 ReactDOM.render(
   // Insert the post component into the DOM
-  <Batch url='/api/v1/posts/'/>,
-  document.getElementById('reactEntry'),
+  <Batch url="/api/v1/posts/" />,
+  document.getElementById("reactEntry")
 );
-
-// function showPosts() {
-
-//   const entry = document.getElementById('reactEntry');
-
-//   function handleResponse (response) {
-//     if (!response.ok) throw Error(response.statusText);
-//     return response.json();
-//   }
-
-//   function handleError(error) {
-//       console.log(error);
-//   }
-
-//   function handleData(data) {
-
-
-//     const posts = data.results;
-//     posts.forEach((post) => {
-    
-//       // create item.owner href `${post.ownerShowUrl}`
-//       const profileName = document.createElement('a');
-//       profileName.href = 'post.ownerShowUrl';
-      
-//       // post header pfp
-//       const ownerImgUrl = document.createElement('img');
-//       // ownerImgUrl.className = 'pfp';
-//       ownerImgUrl.src = 'post.ownerImgUrl';
-//       ownerImgUrl.alt = 'post.ownerImgUrl-post';
-//       profileName.appendChild(ownerImgUrl); // apprending the first part here
-//       entry.appendChild(profileName);
-
-//       // post header username
-//       const ownerUsername = document.createElement('a');
-//       ownerUsername.href = 'post.ownerShowUrl';
-//       ownerUsernameText = document.createTextNode('post.ownerShowUrl');
-//       ownerUsername.appendChild(ownerUsernameText);
-//       entry.appendChild(ownerUsername);
-      
-
-//       // post header time
-//       const postTimestamp = document.createElement('a');
-//       postTimestamp.href = 'post.postShowUrl';
-//       postTimestampText = document.createTextNode(moment('post.created', 'YYYY-MM-DD hh:mm:ss'));
-//       postTimestamp.appendChild(postTimestampText);
-//       entry.appendChild(postTimestamp);
-
-//       // post picture 
-//       const postPicture = document.createElement('img');
-//       postPicture.src = 'post.imgUrl';
-//       ownerImgUrl.alt = 'post.ownerImgUrl-post';
-//       entry.appendChild(postPicture);
-
-//       // remaining stuff is like and comment boxes.
-//     });
-//   }
-
-
-//   fetch("/api/v1/posts/1/")
-//     .then(handleResponse)
-//     .then(handleData)
-//     .catch(handleError);
-// }
-
--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import PropTypes, { string } from 'prop-types';
-import moment from 'moment';
-
+import React from "react";
+import PropTypes, { string } from "prop-types";
+import moment from "moment";
 
 // might need to make a put resquest here as the data gets updated
 function trigger_like(data) {
   if (data.self_like) {
     data.likes = data.likes - 1;
     data.self_like = false;
-  }
-  else {
+  } else {
     data.likes = data.likes + 1;
     data.self_like = true;
   }
@@ -38,25 +36,22 @@ function trigger_like(data) {
 // decides wether to have a delete button
 function serve_delete_button(lognameOwnsThis) {
   if (lognameOwnsThis) {
-    return (
-      <button className="delete-comment-button">
-        delete
-      </button>
-    );
+    return <button className="delete-comment-button">delete</button>;
   }
 }
 
 // displays the comments
 function display_comments(comments) {
-  const listItems = comments.map((d) => 
-  <div key={d.commentid}>
-    <a href={d.ownerShowUrl} className="each-comment" key={d.text}>
-      <strong>{d.owner}</strong> {d.text}
-    </a>
-    {serve_delete_button(d.lognameOwnsThis)}
-  </div>);
+  const listItems = comments.map((d) => (
+    <div key={d.commentid}>
+      <a href={d.ownerShowUrl} className="each-comment" key={d.text}>
+        <strong>{d.owner}</strong> {d.text}
+      </a>
+      {serve_delete_button(d.lognameOwnsThis)}
+    </div>
+  ));
 
-  return (<div>{listItems}</div>);
+  return <div>{listItems}</div>;
 }
 
 class Post extends React.Component {
@@ -66,30 +61,35 @@ class Post extends React.Component {
   constructor(props) {
     // Initialize mutable state
     super(props);
-    this.state = { imgUrl: '', 
-    owner: '', timeStamp: '', 
-    ownerUrl: '', ownerImgUrl: '', 
-    postShowUrl: '', likes: '', 
-    lognameLikesThis: true, postid: '', 
-    comments: [], likeid: '', inputText: ''}
+    this.state = {
+      imgUrl: "",
+      owner: "",
+      timeStamp: "",
+      ownerUrl: "",
+      ownerImgUrl: "",
+      postShowUrl: "",
+      likes: "",
+      lognameLikesThis: true,
+      postid: "",
+      comments: [],
+      likeid: "",
+      inputText: "",
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
-  
 
   componentDidMount() {
-
     // This line automatically assigns this.props.url to the const variable url
     const { url } = this.props;
 
     // Call REST API to get the post's information
-    fetch(url, { credentials: 'same-origin' })
+    fetch(url, { credentials: "same-origin" })
       .then((response) => {
         if (!response.ok) throw Error(response.statusText);
         return response.json();
       })
       .then((data) => {
-
         this.setState({
           imgUrl: data.imgUrl,
           owner: data.owner,
@@ -99,236 +99,250 @@ class Post extends React.Component {
           postShowUrl: data.postShowUrl,
           likes: data.likes.numLikes,
           lognameLikesThis: data.likes.lognameLikesThis,
-          postid: data.postid, comments: [...data.comments], 
-          likeid: data.likes.url
+          postid: data.postid,
+          comments: [...data.comments],
+          likeid: data.likes.url,
         });
       })
       .catch((error) => console.log(error));
   }
 
-//////////////////////////////////////////////helper functions//////////////////////////////////////////////
+  //////////////////////////////////////////////helper functions//////////////////////////////////////////////
   // determines the text on the like button
   like_vs_unlike = (self_like_in) => {
     if (self_like_in) {
-      return 'unlike';
+      return "unlike";
+    } else {
+      return "like";
     }
-    else {
-      return 'like';
-    }
-  }
+  };
 
   // determines wordage attached to num likes
   sing_like_vs_plur_likes = (likes_in) => {
     if (likes_in === 1) {
-      return likes_in + ' like';
+      return likes_in + " like";
+    } else {
+      return likes_in + " likes";
     }
-    else {
-      return likes_in + ' likes';
-    }
-  }
+  };
 
   // decides wether to have a delete button
   serve_delete_button = (lognameOwnsThis, commentid) => {
     if (lognameOwnsThis) {
       return (
-        <button className="delete-comment-button" onClick={() => this.deleteComment(commentid)}>
+        <button
+          className="delete-comment-button"
+          onClick={() => this.deleteComment(commentid)}
+        >
           delete
         </button>
       );
     }
-  }
+  };
 
- // displays the comments
- display_comments = (comments) => {
-  const listItems = comments.map((d) => 
-  <div key={d.commentid} className="each-comment">
-     
-    <a href={d.ownerShowUrl} key={d.commentid}>
-      <strong>{d.owner}</strong>&nbsp;{d.text}
-    </a>
-    {this.serve_delete_button(d.lognameOwnsThis, d.commentid)}
-  </div>);
+  // displays the comments
+  display_comments = (comments) => {
+    const listItems = comments.map((d) => (
+      <div key={d.commentid} className="each-comment">
+        <a href={d.ownerShowUrl} key={d.commentid}>
+          <strong>{d.owner}</strong>&nbsp;{d.text}
+        </a>
+        {this.serve_delete_button(d.lognameOwnsThis, d.commentid)}
+      </div>
+    ));
 
-  return (<div>{listItems}</div>);
-}
+    return <div>{listItems}</div>;
+  };
 
-// removes vals from arr_in
-shrink_array = (arr_in, commentid_in) => {
-  for (var i = arr_in.length - 1; i >= 0; --i) {
-    if (arr_in[i].commentid == commentid_in) {
-        arr_in.splice(i,1);
+  // removes vals from arr_in
+  shrink_array = (arr_in, commentid_in) => {
+    for (var i = arr_in.length - 1; i >= 0; --i) {
+      if (arr_in[i].commentid == commentid_in) {
+        arr_in.splice(i, 1);
+      }
     }
-  }
-}
+  };
 
-
-
-
-
-//////////////////////////////////////////////like/unlike functions//////////////////////////////////////////////
-  
-
+  //////////////////////////////////////////////like/unlike functions//////////////////////////////////////////////
 
   // handles the click of the like button
   handleLike = () => {
     if (this.state.lognameLikesThis) {
       let deleter = this.state.likeid;
-      fetch(deleter, {credentials: 'same-origin', method: 'DELETE'}) 
-      .then((response) => {
-        if (!response.ok) throw Error (response.statusText);
-        // return response.json();
-      })
-      .catch((error) => console.log(error));
-      this.setState ({
+      fetch(deleter, { credentials: "same-origin", method: "DELETE" })
+        .then((response) => {
+          if (!response.ok) throw Error(response.statusText);
+          // return response.json();
+        })
+        .catch((error) => console.log(error));
+      this.setState({
         likes: this.state.likes - 1,
-        lognameLikesThis: false
+        lognameLikesThis: false,
       });
+    } else {
+      let add_like_url = "api/v1/likes/?postid=" + this.state.postid;
+      fetch(add_like_url, { credentials: "same-origin", method: "POST" }) // POST REQUEST
+        .then((response) => {
+          if (!response.ok) throw Error(response.statusText);
+          return response.json();
+        })
+        .then((data) => {
+          this.setState({
+            likes: this.state.likes + 1,
+            lognameLikesThis: true,
+            likeid: "/api/v1/likes/" + data.likeid + "/",
+          });
+        })
+        .catch((error) => console.log(error));
     }
-    else {
-      let add_like_url = 'api/v1/likes/?postid=' + this.state.postid
-      fetch(add_like_url, {credentials: 'same-origin', method: 'POST'}) // POST REQUEST
-      .then((response) => {
-        if (!response.ok) throw Error (response.statusText);
-        return response.json();
-      })
-      .then((data) => {
-        this.setState ({
-          likes: this.state.likes + 1,
-          lognameLikesThis: true,
-          likeid: '/api/v1/likes/' + data.likeid + '/'
-        });
-      })
-      .catch((error) => console.log(error));
-    }
-  }
+  };
 
   // allows the double clicking of an image
   // OPtional add a like animation
   handleDoubleClick = () => {
-    if (this.state.lognameLikesThis) {}
-    else {
-      let add_like_url = 'api/v1/likes/?postid=' + this.state.postid
-      fetch(add_like_url, {credentials: 'same-origin', method: 'POST'}) // POST REQUEST
+    if (this.state.lognameLikesThis) {
+    } else {
+      let add_like_url = "api/v1/likes/?postid=" + this.state.postid;
+      fetch(add_like_url, { credentials: "same-origin", method: "POST" }) // POST REQUEST
         .then((response) => {
-          if (!response.ok) throw Error (response.statusText);
+          if (!response.ok) throw Error(response.statusText);
           return response.json();
         })
         .then((data) => {
-          this.setState ({
+          this.setState({
             likes: this.state.likes + 1,
             lognameLikesThis: true,
-            likeid: '/api/v1/likes/' + data.likeid + '/'
+            likeid: "/api/v1/likes/" + data.likeid + "/",
           });
           // console.log("via delte url---" this.state.likeid);
         })
         .catch((error) => console.log(error));
     }
-  }
-//////////////////////////////////////COMMENTS FUNCTIONS///////////////////////////////////////////
-  handleChange(event) {    
-    this.setState({inputText: event.target.value});  
+  };
+  //////////////////////////////////////COMMENTS FUNCTIONS///////////////////////////////////////////
+  handleChange(event) {
+    this.setState({ inputText: event.target.value });
   }
 
   createComment = (event) => {
     event.preventDefault();
-    let comments_url = 'api/v1/comments/?postid=' + this.state.postid;
+    let comments_url = "api/v1/comments/?postid=" + this.state.postid;
     fetch(comments_url, {
-      'Content-Type': 'application/json',
-      credentials: 'same-origin', 
-      method: 'POST',
+      "Content-Type": "application/json",
+      credentials: "same-origin",
+      method: "POST",
       body: JSON.stringify({
-        text: this.state.inputText})
+        text: this.state.inputText,
+      }),
     }) // POST REQUEST
       .then((response) => {
-        if (!response.ok) throw Error (response.statusText);
+        if (!response.ok) throw Error(response.statusText);
         return response.json();
       })
       .then((data) => {
-        this.setState ({
+        this.setState({
           comments: this.state.comments.concat({
-            'text': this.state.inputText, 
-            'owner': data.owner, 
-            'ownerShowUrl': data.ownerShowUrl,
-            'lognameOwnsThis': data.lognameOwnsThis,
-            'commentid': data.commentid,
-            'url': data.url
-          }), inputText: ''
+            text: this.state.inputText,
+            owner: data.owner,
+            ownerShowUrl: data.ownerShowUrl,
+            lognameOwnsThis: data.lognameOwnsThis,
+            commentid: data.commentid,
+            url: data.url,
+          }),
+          inputText: "",
         });
         // console.log("via delte url---" this.state.likeid);
         console.log(data);
       })
       .catch((error) => console.log(error));
-  }
+  };
 
   // deletes a comment
   deleteComment = (commentid_in) => {
     console.log("entered here");
     console.log(commentid_in);
-    let deleter = '/api/v1/comments/' + commentid_in + '/';
-      fetch(deleter, {credentials: 'same-origin', method: 'DELETE'}) 
+    let deleter = "/api/v1/comments/" + commentid_in + "/";
+    fetch(deleter, { credentials: "same-origin", method: "DELETE" })
       .then((response) => {
-        if (!response.ok) throw Error (response.statusText);
+        if (!response.ok) throw Error(response.statusText);
         // return response.json();
       })
       .catch((error) => console.log(error));
-      this.shrink_array(this.state.comments, commentid_in);
-      this.setState ({
-        comments: this.state.comments
-      });
-  }
-
-
-
+    this.shrink_array(this.state.comments, commentid_in);
+    this.setState({
+      comments: this.state.comments,
+    });
+  };
 
   render() {
     // This line automatically assigns this.state.imgUrl to the const variable imgUrl
     // and this.state.owner to the const variable owner
-    const { imgUrl, owner, timeStamp, ownerUrl, ownerImgUrl, postShowUrl, likes, lognameLikesThis, comments, postid, inputText} = this.state;
+    const {
+      imgUrl,
+      owner,
+      timeStamp,
+      ownerUrl,
+      ownerImgUrl,
+      postShowUrl,
+      likes,
+      lognameLikesThis,
+      comments,
+      postid,
+      inputText,
+    } = this.state;
 
     // Render number of post image and post owner
     return (
       <div key={postid} className="post">
         <div className="wrapper">
-            <div className="container">
-                <a href={ownerUrl}>
-                    <img className="pfp" src={ownerImgUrl} alt="profile-picture"/>
-                </a>
-                <div className="account">
-                    <a href={ownerUrl} className="username">
-                        <p><strong>{owner}</strong></p>
-                    </a>
-                    <a href={postShowUrl} className="time">
-                        <p>{timeStamp}</p>
-                    </a>
-                </div>
+          <div className="container">
+            <a href={ownerUrl}>
+              <img className="pfp" src={ownerImgUrl} alt="profile-picture" />
+            </a>
+            <div className="account">
+              <a href={ownerUrl} className="username">
+                <p>
+                  <strong>{owner}</strong>
+                </p>
+              </a>
+              <a href={postShowUrl} className="time">
+                <p>{timeStamp}</p>
+              </a>
             </div>
-            <div className="picture" onDoubleClick={this.handleDoubleClick}>
-                <img src={imgUrl} alt="image"/>
+          </div>
+          <div className="picture" onDoubleClick={this.handleDoubleClick}>
+            <img src={imgUrl} alt="image" />
+          </div>
+          <div className="container-bottom">
+            {/* like and unlike button */}
+            <div className="like">
+              <button
+                className="like-unlike-button"
+                onClick={this.handleLike}
+                type="button"
+              >
+                {this.like_vs_unlike(lognameLikesThis)}
+              </button>
+              {/* number of likes */}
+              <div className="likes">{this.sing_like_vs_plur_likes(likes)}</div>
             </div>
-            <div className="container-bottom">
-              {/* like and unlike button */}
-              <div className="like">
-                <button className="like-unlike-button" onClick={this.handleLike} type="button">
-                  {this.like_vs_unlike(lognameLikesThis)}
-                </button>
-                {/* number of likes */}
-                <div className="likes">
-                  {this.sing_like_vs_plur_likes(likes)}
-                </div>
-              </div>
-              {/* generated comments */}
-              <div className="comments">
-                {this.display_comments(comments)}
-              </div>
-              {/* replies */}
-              <div className="reply">
-                <form className="comment-form" onSubmit={this.createComment}>
-                  <input className="comment" type="text" placeholder="Add a comment..." value={inputText} onChange={this.handleChange}/>
-                </form>
-              </div>
+            {/* generated comments */}
+            <div className="comments">{this.display_comments(comments)}</div>
+            {/* replies */}
+            <div className="reply">
+              <form className="comment-form" onSubmit={this.createComment}>
+                <input
+                  className="comment"
+                  type="text"
+                  placeholder="Add a comment..."
+                  value={inputText}
+                  onChange={this.handleChange}
+                />
+              </form>
             </div>
+          </div>
         </div>
-      </div>  
+      </div>
     );
   }
 }
