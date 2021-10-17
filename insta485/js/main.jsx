6f207a88b8539c5c82da6029ddc3052ce826bb3d
storke <storke@umich.edu>
2021-10-16 22:34:49 -0400

    jump to top?

--- insta485/js/main.jsx
+++ insta485/js/main.jsx
@@ -4,6 +4,12 @@ import Batch from './middle';
 
 // This method is only called once
 // {/* <Post url="/api/v1/posts/1/" />, */}
+
+window.onbeforeunload = function () {
+  console.log("jumping to top");
+  window.scrollTo(0, 0);
+}
+
 ReactDOM.render(
   // Insert the post component into the DOM
   <Batch url="/api/v1/posts/" />,
